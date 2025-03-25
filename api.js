170855efc72b98b65710e5ec6fba644ebd1b229a
Hient <hient@usf.edu>
2025-03-25 16:42:57 -0400

    DONE

--- api.js
+++ api.js
@@ -1,22 +1,22 @@
 const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
 export async function getPosts() {
     try {
-        const res = await fetch (BASE_URL);
+        const res = await fetch(BASE_URL);
         if (!res.ok){
             throw new Error (`Error: ${res.status}`);
         }
         return await res.json();
     } catch (err){
-     console.error('API Fetch Failed:',err.message);
+     console.error('API Fetch Failed:', err.message);
      throw err;
      }
 }
-import{getPosts} from './api.js';
-async function displayPosts();{
-    const container = document.getElementById('postCantainter');
+import { getPosts } from './api.js';
+async function displayPosts() {
+    const container = document.getElementById('postContainer');
     try {
         const posts = await getPosts();
-        posts.slice(0, 5). forEach(post=>{
+        posts.slice(0, 5). forEach(post => {
             const div = document.createElement('div');
             div.innerHTML = `<h3>$(post.title)</h3><p>${post.body}</p>`;
             container.appendChild(div);
